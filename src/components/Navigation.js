4cff498689bb551e444cdb3b4ce6b61d70e200a6
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-06-21 18:24:17 -0500

    navigation styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,61 +1,24 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-const linkStyles = {
-  display: "inline-block",
-  width: "50px",
-  padding: "12px",
-  margin: "0 6px 6px",
-  background: "blue",
-  textDecoration: "none",
-  color: "white",
-};
 
 function Navigation() {
   return (
-    <div>
-      <h1>Post Records</h1>
-      <NavLink 
-        to="/"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
+    <nav>
+    <div class="site-title">Post-Records</div>
+      <NavLink exact to="/">
         Home
       </NavLink>
-      <NavLink
-        to="/about"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
-        About
-      </NavLink>
-      <NavLink
-        to="/shop"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
+      <NavLink exact to="/shop">
         Shop
       </NavLink>
-      <NavLink
-        to="/collection"
-        exact
-        style={linkStyles}
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
+      <NavLink exact to="/collection">
         Collection
       </NavLink>
-    </div>
+      <NavLink exact to="/about">
+        About
+      </NavLink>
+    </nav>
   );
 }
 

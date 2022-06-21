13823bd4ce4fb4e408ef1cc16ba13cd9a4b570aa
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-06-21 12:11:25 -0500

    add record.js component

--- src/App.js
+++ src/App.js
@@ -9,6 +9,10 @@ const App = () => {
       <div className = "App">
        <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+        <Route exact path="/collection" component={Coollection} />
+        <Route exact path="/about" component={About} />
+        <Route path="/ship/:id" component={Record} />
        </Switch>
       </div>
     </Router>

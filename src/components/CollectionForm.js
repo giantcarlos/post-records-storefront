a3d79bf7b5fdbf199aefa6ac993c6d3802dead5c
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-06-30 13:13:01 -0500

    add form css

--- src/components/CollectionForm.js
+++ src/components/CollectionForm.js
@@ -2,9 +2,31 @@ import React from 'react'
 
 const CollectionForm = () => {
   return (
-    <form>CollectionForm
-
-        
+    <form className="collectionForm">
+        <h3>Submit a Record For Your Collection</h3>
+        <div className="formText">
+            <label>Album Title:
+                <input type="text"/>
+            </label><br></br>
+            <label>Artist Name: 
+                <input type="text"/>
+            </label><br></br>
+            <label>Comment: 
+                <input type="text"/>
+            </label><br></br>
+            <label>Release Year: 
+                <input type="text"/>
+            </label><br></br>
+            <label>Record Label: 
+                <input type="text"/>
+            </label><br></br>
+            <label>Catalog Number: 
+                <input type="text"/>
+            </label><br></br>
+            <label>Image URL: 
+                <input type="text"/>
+            </label>
+        </div>
     </form>
   )
 }

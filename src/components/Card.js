6d320fd9c8b73e5cd3356a15990e5c9f3759985c
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-06-29 20:11:16 -0500

    Card component minor edit

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,15 @@ const Card = ({ id, record }) => {
 
     return (
         <Link to={`/shop/${id}/`}>
-         <div className="card">
-            <div className="card-image">
-                <img src={image} alt="cover"/>
+            <div className="card">
+                <div className="card-image">
+                    <img src={image} alt="cover"/>
+                </div>
+                <div className="card-album">{album}</div>
+                <div className="card-details">{artist}</div>
+                <div className="card-details">${price}</div>
+                <div className="card-details">{comment}</div>
             </div>
-            <div className="card-album">{album}</div>
-            <div className="card-details">{artist}</div>
-            <div className="card-details">${price}</div>
-            <div className="card-details">{comment}</div>
-        </div>
         </Link>
     )
 }

fcc950a04f3f7ede434e8ea28f23baf623a6791b
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-06-30 13:49:48 -0500

    edit form CSS

--- src/components/CollectionForm.js
+++ src/components/CollectionForm.js
@@ -1,30 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const CollectionForm = () => {
+    const [form, setForm] = useState("");
+        
+
   return (
     <form className="collectionForm">
         <h3>Submit a Record For Your Collection</h3>
         <div className="formText">
             <label>Album Title:
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Artist Name: 
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Comment: 
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Release Year: 
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Record Label: 
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Catalog Number: 
-                <input type="text"/>
-            </label><br></br>
+                <input type="textarea"/>
+            </label>
             <label>Image URL: 
-                <input type="text"/>
+                <input type="textarea"/>
             </label>
         </div>
     </form>
